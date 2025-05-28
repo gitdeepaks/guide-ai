96a32be504c7ffb2279b1e3fc8103d4a923c947d
gitdeepaks <deepaksankhyan92@outlook.com>
2025-05-28 09:37:49 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <Button variant="outline" className="text-4xl text-green-500">
+      Hello AI
+    </Button>
+  );
+}
